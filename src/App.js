39afb353997e89f724d7b3dcb8a3ada5b322b458
ahmed <ahmedmostafa05031991@gmail.com>
2019-05-14 08:01:28 +0200

    add redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
 import Routes from './RootNavigator';
 
 export default class App extends Component {
     render() {
         return (
-            <Routes />
+            <Provider store={createStore(reducer)}>
+                <Routes />
+            </Provider>
         );
     }
 }
